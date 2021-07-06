08af1314e3d49738079a8a106b2ea55912ff7208
Alex1989-star <bio181@yandex.ru>
2021-07-06 10:46:41 +0300

    Initial Commit

---
+++ CoreDataDemoo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemoo
+//
+//  Created by Леся on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
